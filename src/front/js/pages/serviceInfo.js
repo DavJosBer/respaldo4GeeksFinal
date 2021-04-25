f2fe7c530e4f715113931e979d4635c14d4bb5a2
Jobanny Zúñiga <jobannyz@gmail.com>
2021-04-25 15:53:44 +0000

    First Commit

--- src/front/js/pages/serviceInfo.js
+++ src/front/js/pages/serviceInfo.js
@@ -13,24 +13,19 @@ export const ServiceInfo = () => {
 				{store.services.length > 0 && (
 					<>
 						<h1>{store.services[params.theid].name}</h1>
+						<p>Bocadillos: {store.services[params.theid].Bocadillos}</p>
+						<p>Entrada: {store.services[params.theid].Entrada}</p>
+						<p>Plato Fuerte: {store.services[params.theid].Plato_Fuerte}</p>
+						<p>{store.services[params.theid].Ensalada}</p>
+						<p>{store.services[params.theid].Bebida}</p>
+						<p>{store.services[params.theid].Postre}</p>
+						<p>{store.services[params.theid].Decoración}</p>
+						<p>{store.services[params.theid].DJ}</p>
 						<p>{store.services[params.theid].precio}</p>
-						<p>{store.services[params.theid].catering_service}</p>
-						<p>{store.services[params.theid].decoracion}</p>
-						<p>{store.services[params.theid].dj}</p>
 					</>
 				)}
 			</div>
-			<Button
-				variant="primary"
-				onClick={() => {
-					actions.addFavorite(
-						event,
-						store.services[params.theid].name,
-						store.services[params.theid].precio,
-						store.services[params.theid].id
-					);
-					console.log(store.favorites);
-				}}>
+			<Button variant="primary" onClick={() => actions.addFavorite(event, store.services[params.theid].name)}>
 				Agregar al Carrito
 			</Button>
 		</>
